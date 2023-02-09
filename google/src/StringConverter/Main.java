df37330ed00cb894436975907543a035bb9f487a
Zicheng Liu <ziliu@purestorage.com>
2023-02-09 09:46:52 -0800

    extend the num beyond 10

--- google/src/StringConverter/Main.java
+++ google/src/StringConverter/Main.java
@@ -9,25 +9,31 @@ import java.util.Stack;
 // reverse : 2)4B(3A
 
 
-// Input: (A3B2)2
-// reverse 2)2B3A(
+// Input: (A3B2)21
+// reverse 12)2B3A(
 public class Main {
     public static Map<Character, Integer> convert(String input) {
         Map<Character, Integer> result = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         int multiplier = 1;
-        int k = 0;
         for (int i = input.length() - 1; i >= 0; i--) {
             char c = input.charAt(i);
             if (c >= '0' && c <= '9') {
                 int number = c - '0';
                 stack.push(number);
             } else if (c >= 'A' && c <= 'Z') {
-                k += stack.pop();
-                result.put(c, result.getOrDefault(c, 0) + k * multiplier);
-                k = 0;
+                int num = stack.pop();
+                while (!stack.isEmpty()) {
+                    num = num * 10 + stack.pop();
+                }
+                result.put(c, result.getOrDefault(c, 0) + num * multiplier);
             } else if (c == ')') {
-                multiplier *= stack.pop();
+                int num = stack.pop();
+                while (!stack.isEmpty()) {
+                    num = num * 10 + stack.pop();
+                }
+
+                multiplier *= num;
             } else if (c == '(') {
                 multiplier = 1;
             }
@@ -40,7 +46,7 @@ public class Main {
         System.out.println("Input: " + input1);
         System.out.println("Output: " + convert(input1));
 
-        String input2 = "((A3)2B2)2";
+        String input2 = "(A31B2)21";
         System.out.println("Input: " + input2);
         System.out.println("Output: " + convert(input2));
 
